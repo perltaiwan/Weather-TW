581debb53a292a45633329b1c3aae3eaaeae0254
dryman <idryman@gmail.com>
2011-05-09 13:51:44 +0800

    init commit

---
+++ lib/Weather/TW.pm
@@ -0,0 +1,63 @@
+package Weather::TW;
+
+use 5.008006;
+use strict;
+use warnings;
+use Carp;
+
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Weather::TW - Fetch Taiwan weather data from 中央氣象局
+
+=head1 SYNOPSIS
+
+  use Weather::TW;
+
+=head1 DESCRIPTION
+
+Stub documentation for Weather::TW, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+dryman, E<lt>dryman@apple.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by dryman
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
+1;
+__END__
