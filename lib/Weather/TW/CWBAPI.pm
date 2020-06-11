1e0f8760f6ab9706e1627bdb67c13690787fa0d2
Kang-min Liu <gugod@gugod.org>
2020-06-11 19:59:19 +0800

    documentation

--- lib/Weather/TW/CWBAPI.pm
+++ lib/Weather/TW/CWBAPI.pm
@@ -34,3 +34,38 @@ package Weather::TW::CWBAPI {
 };
 
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Weather::TW::CWBAPI - Accessing CWB WebAPI
+
+=head1 DESCRIPTION
+
+This is a thin client for CWB WebAPI. Users of this class needs to
+know the details about CWB WebAPI in order to use this class
+correctly.
+
+=head1 Attributes
+
+=head2 authorization_key
+
+This attribute is mandatory and is need for to make requests. The provided
+value is automatically added to every outgoing requests.
+
+=head2 api_base
+
+This is a constant C<"https://opendata.cwb.gov.tw/"> and probably
+won't need to be changed.
+
+=head1 Methods
+
+=head2 get( Str $path, HashRef $query_param ) #=> Mojo::Transaction
+
+This method simply re-wrap C<$path> and C<$query_param> and passthrough
+C<Mojo::UserAgent->new->get()>.
+
+=cut
